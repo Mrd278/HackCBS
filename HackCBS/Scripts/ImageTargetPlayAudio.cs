db6df8867e2649b49f0012e0ac034eb282279ede
Mridul Gupta <47922198+Mrd278@users.noreply.github.com>
2019-10-20 11:31:23 +0530

    Add files via upload

---
+++ HackCBS/Scripts/ImageTargetPlayAudio.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using Vuforia;
+
+public class ImageTargetPlayAudio : MonoBehaviour, ITrackableEventHandler
+{
+    private TrackableBehaviour mTrackableBehaviour;
+    public GameObject Quad;
+    private Animator anim;
+    //private MovieTexture movie;
+
+    void Start()
+    {
+        mTrackableBehaviour = GetComponent<TrackableBehaviour>();
+        if (mTrackableBehaviour)
+        {
+            mTrackableBehaviour.RegisterTrackableEventHandler(this);
+        }
+        anim = Quad.GetComponent<Animator>();
+        //movie = Quad.GetComponent<MovieTexture>();
+        //movie.Pause();
+    }
+
+    public void OnTrackableStateChanged(
+                                    TrackableBehaviour.Status previousStatus,
+                                    TrackableBehaviour.Status newStatus)
+    {
+        if (newStatus == TrackableBehaviour.Status.DETECTED ||
+            newStatus == TrackableBehaviour.Status.TRACKED ||
+            newStatus == TrackableBehaviour.Status.EXTENDED_TRACKED)
+        {
+            anim.Play("Book");
+            //movie.Play();
+        }
+        else
+        {
+            //movie.Pause();
+            return;
+        }
+    }
+}
---
+++ HackCBS/Scripts/Menu.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void Begin()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
